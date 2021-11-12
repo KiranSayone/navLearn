3a17ad0f14fdbbdc88548724d434fead3812d02e
sayone <sayone@sayones-iMac-Pro.local>
2021-11-12 18:32:25 +0530

    landing page design

--- src/modules/parentLandingScreen/parentLandingScreen.js
+++ src/modules/parentLandingScreen/parentLandingScreen.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Image, Button} from 'react-native';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import styles from './styles';
 
 const parentLandingScreen = ({navigation}) => {
+  const recommendedChallenge = [
+    {
+      id: '1',
+      subject: 'Social Science',
+      title: 'Cold War',
+    },
+    {
+      id: '2',
+      subject: 'Mathematics',
+      title: 'Algebra',
+    },
+    {
+      id: '3',
+      subject: 'Science',
+      title: 'Photosynthesis',
+    },
+    {
+      id: '4',
+      subject: 'Physics',
+      title: 'Pendulum',
+    },
+  ];
+  const CardRender = ({item}) => {
+    return (
+      <View style={styles.cardContainer}>
+        <Text style={styles.challengeSubject}>{item.subject}:</Text>
+        <Text style={styles.challengeTitle}>{item.title}</Text>
+      </View>
+    );
+  };
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.headerText}>Welcome back James!</Text>
-      <Button
-        title="Go to next screen."
-        onPress={() => {
-          navigation.navigate('Statistics');
-        }}
-      />
-    </View>
+    <ScrollView contentContainerStyle={styles.mainContainer}>
+      <View style={styles.headerContainer}></View>
+      <View style={styles.recommendedChallenge}>
+        <Text style={styles.recommendedText}>Recommended Challenges</Text>
+        <FlatList
+          data={recommendedChallenge}
+          renderItem={CardRender}
+          keyExtractor={item => item.id}
+          horizontal={true}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
--- src/modules/parentLandingScreen/styles.js
+++ src/modules/parentLandingScreen/styles.js
@@ -3,10 +3,49 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   mainContainer: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  headerText: {
+  recommendedText: {
+    flex: 0,
+    marginTop: 20,
+    marginLeft: 20,
     fontSize: 30,
+  },
+  headerContainer: {
+    flex: 2,
+    width: '100%',
+    backgroundColor: '#78a8fa',
+  },
+  cardContainer: {
+    alignContent: 'center',
+    width: 210,
+    height: 230,
+    margin: 20,
+    borderRadius: 15,
+    backgroundColor: '#246ae3',
+  },
+  safeArea: {
+    padding: 20,
+  },
+  recommendedChallenge: {
+    flex: 1.5,
+  },
+  cardSubject: {
+    color: 'black',
+    fontSize: 10,
+  },
+  challengeSubject: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  challengeTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 10,
   },
 });
